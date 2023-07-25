471d9ae0402d069ca119615a961694a577f3724b
xaman1990 <carlosnavarron1990@gmail.com>
2023-07-24 19:44:39 -0500

    Change DTO

---
+++ Shared/DataTransferObjects/RepartoDto.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shared.DataTransferObjects
+{
+    public class RepartoDto
+    {
+        public Guid Id { get; set; }     
+        public Guid ClienteId { get; set; }
+        public Guid TrabajadorId { get; set; }
+        public DateTime FechaEntrega { get; set; }
+        public string? Comentario { get; set; }
+        public string? EstadoReparto { get; set; }
+        public string? Estado { get; set; }
+    }
+}
