e6ce5eced243552fd0c3814e3387c78a538f346c
John C <aljo1915@gmail.com>
2023-08-07 00:03:43 -0500

    Add docker and docker-compose support to the webapi project. Install and set up serilog. Refactor services as a service extension. Create Global exception middleware extension. Remove built-in error handler of asp net core.

--- BlueLife/Program.cs
+++ BlueLife/Program.cs
@@ -1,46 +1,53 @@
-using Contracts.IRepository;
-using Contracts.IService;
+using BlueLife.ActionFilters;
+using BlueLife.Extensions;
+using Contracts;
 using Microsoft.AspNetCore.HttpOverrides;
-using Microsoft.EntityFrameworkCore;
-using Microsoft.Extensions.Configuration;
-using Repository;
-using Service;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Formatters;
+using Microsoft.Extensions.Options;
 
 namespace BlueLife
 {
     public class Program
     {
+        private const string _CORS_VALUE = "CorsPolicy";
         public static void Main(string[] args)
         {
             var builder = WebApplication.CreateBuilder(args);
             // Add services to the container.
             builder.Services.AddAutoMapper(typeof(Program));
 
-            builder.Services.AddDbContext<RepositoryContext>(options =>
-            options.UseMySql(builder.Configuration.GetConnectionString("DefaultConnection"),
-            ServerVersion.AutoDetect(builder.Configuration.GetConnectionString("DefaultConnection")),
-            b => b.MigrationsAssembly("BlueLife")));
+            builder.Services.ConfigureCors();
 
-            builder.Services.AddCors(options =>
+            builder.Services.ConfigureIISIntegration();
+
+            builder.Host.ConfigureSerilog();
+
+            builder.Services.ConfigureLoggerService();
+
+            builder.Services.ConfigureDbContext(builder.Configuration);
+
+            builder.Services.ConfigureRepositoryManager();
+
+            builder.Services.ConfigureServiceManager();
+
+            builder.Services.Configure<ApiBehaviorOptions>(options =>
             {
-                options.AddPolicy("CorsPolicy", builder =>
-                builder.AllowAnyOrigin()
-                .AllowAnyMethod()
-                .AllowAnyHeader()
-                .WithExposedHeaders("X-Pagination"));
+                options.SuppressModelStateInvalidFilter = true;
             });
+            builder.Services.AddScoped<ValidationFilterAttribute>();
 
-            builder.Services.Configure<IISOptions>(options => { });
-
-            builder.Services.AddScoped<IRepositoryManager, RepositoryManager>();
-
-            builder.Services.AddScoped<IServiceManager, ServiceManager>();
-
-            builder.Services.AddControllers();
+            builder.Services.AddControllers(config =>
+            {
+                config.RespectBrowserAcceptHeader = true;
+                config.ReturnHttpNotAcceptable = true;
+                config.InputFormatters.Insert(0, GetJsonPatchInputFormatter());
+            }).AddXmlDataContractSerializerFormatters();
 
             var app = builder.Build();
-
             // Configure the HTTP request pipeline.
+            var logger = app.Services.GetRequiredService<ILoggerManager>();
+            app.ConfigureExceptionHandler(logger);
 
             if (app.Environment.IsProduction())
                 app.UseHsts();
@@ -49,12 +56,12 @@ namespace BlueLife
 
             app.UseStaticFiles();
 
-            app.UseCors("CorsPolicy");
-
             app.UseForwardedHeaders(new ForwardedHeadersOptions
             {
                 ForwardedHeaders = ForwardedHeaders.All
             });
+
+            app.UseCors(_CORS_VALUE);
 
             app.UseAuthorization();
 
@@ -62,5 +69,12 @@ namespace BlueLife
 
             app.Run();
         }
+
+        //This method is used to allow patch-json type media
+        private static NewtonsoftJsonPatchInputFormatter GetJsonPatchInputFormatter() =>
+                    new ServiceCollection().AddLogging().AddMvc().AddNewtonsoftJson()
+                    .Services.BuildServiceProvider()
+                    .GetRequiredService<IOptions<MvcOptions>>().Value.InputFormatters
+                    .OfType<NewtonsoftJsonPatchInputFormatter>().First();
     }
 }
---
+++ LoggerService/LoggerManager.cs
@@ -0,0 +1,18 @@
+﻿using Contracts;
+using Serilog;
+
+namespace LoggerService
+{
+    public class LoggerManager : ILoggerManager
+    {
+        private static readonly ILogger _logger = Log.ForContext<LoggerManager>();
+        public LoggerManager() { }
+        public void LogDebug(string message) => _logger?.Debug(message);
+
+        public void LogError(string message) => _logger?.Error(message);
+
+        public void LogInfo(string message) => _logger?.Information(message);
+
+        public void LogWarn(string message) => _logger?.Warning(message);
+    }
+}
